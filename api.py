d9f45a15be120c2864c7268d929c9bff18c5f6e5
gordianoguilherme <guilherme.gordiano@schwarz.com.br>
2025-05-20 11:17:48 -0300

    corrigri

--- api.py
+++ api.py
@@ -9,4 +9,4 @@ def read_root():
     return "Olá mundo!"
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000)    
+    uvicorn.run('api:app', host='0.0.0.0', port=8000)    
