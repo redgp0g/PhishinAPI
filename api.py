696ce00ba1eea8bb5a8ad0d29e4994e5d6609b4e
gordianoguilherme <guilherme.gordiano@schwarz.com.br>
2025-05-20 10:39:12 -0300

    teste

--- api.py
+++ api.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/",response_class=HTMLResponse)
 def read_root():
     return "Olá mundo!"
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000)    
