44132471a7695e88dbc1534021fb704d82dee898
gordianoguilherme <guilherme.gordiano@schwarz.com.br>
2025-05-20 11:23:20 -0300

    Remover parâmetro 'identificador' da função registrar_clique

--- api.py
+++ api.py
@@ -9,13 +9,12 @@ def read_root():
     return "Olá mundo!"
 
 @app.get("/registrar-clique")
-async def registrar_clique(request: Request, nome: str = None, identificador: str = None, ip: str = None):
+async def registrar_clique(request: Request, nome: str = None):
     if not nome:
         raise HTTPException(status_code=400, detail="Parâmetros obrigatórios ausentes")
 
     return JSONResponse(content={
         "mensagem": "Clique registrado com sucesso",
         "nome": nome,
-        "identificador": identificador,
         "ip": request.client.host
     })
